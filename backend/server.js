035d153b22267a5665f095e26f34aaaeed585a12
Zeraijin301 <138296747+ZeroPinzaki301@users.noreply.github.com>
2025-08-29 11:17:30 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import connectDB from "./config/db.js"; 
